8c6a2f5fe183ace553691d9daf26a853f10fe441
Dei Gratia Radrigov Rahawarin <rdeigratia95@gmail.com>
2018-12-06 15:21:07 +0700

    create backend

---
+++ index.js
@@ -0,0 +1,25 @@
+const _ = require('lodash')
+  // var result =[];
+  const names=['pro','gram','merit','program','it','programmer']
+  for ( name = 1; name <= 5 ; name++){
+    console.log(names.splice(names,2))
+  }
+
+
+// const result = _.split(name,',',3)
+// console.log(result);s
+//
+// const result1 = _.split(name,',',2)
+// console.log(result1);
+//
+// const result2 = _.split(name, ',',3)
+// console.log(result2);
+//
+// const result3 = _.split(name, ',', 2)
+// console.log(result3);
+//
+// const result4 = _.split(name, ',', 2)
+// console.log(result4);
+//
+// const result5 = _.split(name,',', 1)
+// console.log(result5)
